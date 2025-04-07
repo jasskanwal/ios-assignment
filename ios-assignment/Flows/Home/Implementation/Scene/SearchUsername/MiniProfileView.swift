b7fcc8fa366cd3d49a0b385e179421aa80064f0e
Jaskirat Singh <jasskanwal@gmail.com>
2025-04-07 00:39:00 -0400

    feat: extract mini profile from list

---
+++ ios-assignment/Flows/Home/Implementation/Scene/SearchUsername/MiniProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  MiniProfileView.swift
+//  ios-assignment
+//
+//  Created by Jaskirat Singh on 2025-04-07.
+//
+
+import SwiftUI
+
+struct MiniProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MiniProfileView()
+}
